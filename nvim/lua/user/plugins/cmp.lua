9d9e0c9898124e98f07dc77727ce619f62b106d5
Tmeister <noone@tmeister.net>
2022-05-05 17:17:33 -0500

    Add Tab completion

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -1,7 +1,8 @@
--- local has_words_before = function()
---     local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
---     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
--- end
+local has_words_before = function()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+end
+
 local luasnip = require 'luasnip'
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
@@ -39,44 +40,18 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = false
         },
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --     if luasnip.jumpable(1) then
-        --         luasnip.jump(1)
-        --     elseif cmp.visible() then
-        --         cmp.select_next_item()
-        --     elseif has_words_before() then
-        --         cmp.complete()
-        --     else
-        --         fallback()
-        --     end
-
-        --     -- if cmp.visible() then
-        --     --   cmp.select_next_item()
-        --     -- elseif luasnip.expand_or_jumpable() then
-        --     --   luasnip.expand_or_jump()
-        --     -- elseif has_words_before() then
-        --     --   cmp.complete()
-        --     -- else
-        --     --   fallback()
-        --     -- end
-        -- end, {'i', 's'}),
-        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-        --     if luasnip.jumpable(-1) then
-        --         luasnip.jump(-1)
-        --     elseif cmp.visible() then
-        --         cmp.select_prev_item()
-        --     else
-        --         fallback()
-        --     end
-
-        --     -- if cmp.visible() then
-        --     --   cmp.select_prev_item()
-        --     -- elseif luasnip.jumpable(-1) then
-        --     --   luasnip.jump(-1)
-        --     -- else
-        --     --   fallback()
-        --     -- end
-        -- end, {'i', 's'})
+       
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            elseif cmp.visible() then
+                cmp.select_next_item()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
     },
     sources = {{
         name = 'nvim_lsp'
