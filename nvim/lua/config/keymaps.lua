a4ecbd7c8a07d206d17aea71958b9dcd3fa642c4
Tmeister <noone@tmeister.net>
2025-08-23 18:46:12 -0600

    Add  effect to the modules

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,86 +2,37 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Disable default behavior of space
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+-- Window navigation is handled by LazyVim defaults (<C-h/j/k/l>)
 
--- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
--- Move lines up and down
--- Normal mode
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+-- Line movement is handled by LazyVim defaults (<A-j/k>)
+-- Adding arrow key alternatives for convenience
 vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-
--- Visual mode
-vim.keymap.set("v", "<A-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move selection up" })
 vim.keymap.set("v", "<A-Down>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "<A-Up>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move selection up" })
-
--- Insert mode
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
 vim.keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
 vim.keymap.set("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
-
--- Better indenting
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
 
 -- Clear search highlighting
 vim.keymap.set("n", "<Esc>", "<cmd>nohl<cr>", { desc = "Clear search highlight" })
 
--- Save file
-vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
+-- Save file is handled by LazyVim defaults (<C-s>)
+-- Adding additional modes for convenience
 vim.keymap.set("i", "<C-s>", "<Esc><cmd>w<cr>a", { desc = "Save file" })
 vim.keymap.set("v", "<C-s>", "<Esc><cmd>w<cr>", { desc = "Save file" })
 
--- macOS specific keymaps using Option/Alt key
--- These work better on macOS terminals
+-- Linux specific keymaps optimized for 75% keyboard
+-- These work better on Linux terminals
 
--- Delete word backward (Option+Backspace)
-vim.keymap.set("i", "<A-BS>", "<C-w>", { desc = "Delete word backward" })
+-- Delete word backward (Ctrl+Backspace - standard Linux)
+vim.keymap.set("i", "<C-BS>", "<C-w>", { desc = "Delete word backward" })
+vim.keymap.set("i", "<C-H>", "<C-w>", { desc = "Delete word backward (alt)" })
 
--- Shift+Arrow for beginning/end of line
-vim.keymap.set("i", "<S-Left>", "<C-o>^", { desc = "Move to beginning of line" })
-vim.keymap.set("i", "<S-Right>", "<C-o>$", { desc = "Move to end of line" })
-vim.keymap.set("n", "<S-Left>", "^", { desc = "Move to beginning of line" })
-vim.keymap.set("n", "<S-Right>", "$", { desc = "Move to end of line" })
-
--- Move word by word
-vim.keymap.set("i", "<A-b>", "<C-o>b", { desc = "Move word backward" })
-vim.keymap.set("i", "<A-f>", "<C-o>w", { desc = "Move word forward" })
-vim.keymap.set("n", "<A-b>", "b", { desc = "Move word backward" })
-vim.keymap.set("n", "<A-f>", "w", { desc = "Move word forward" })
-
--- Delete to end of line
-vim.keymap.set("i", "<A-d>", "<C-o>D", { desc = "Delete to end of line" })
-vim.keymap.set("n", "<A-d>", "D", { desc = "Delete to end of line" })
+-- Delete to end of line (Ctrl+Shift+K to avoid LSP conflicts)
+vim.keymap.set("i", "<C-S-k>", "<C-o>D", { desc = "Delete to end of line" })
 
 -- Better paste
 vim.keymap.set("v", "p", '"_dP', { desc = "Paste without yanking" })
-
--- Duplicate lines
-vim.keymap.set("n", "<A-S-Down>", "<cmd>t.<cr>", { desc = "Duplicate line down" })
-vim.keymap.set("n", "<A-S-Up>", "<cmd>t-1<cr>", { desc = "Duplicate line up" })
-vim.keymap.set("n", "<A-S-j>", "<cmd>t.<cr>", { desc = "Duplicate line down" })
-vim.keymap.set("n", "<A-S-k>", "<cmd>t-1<cr>", { desc = "Duplicate line up" })
-
-vim.keymap.set("v", "<A-S-Down>", "<cmd>t'><cr>gv", { desc = "Duplicate selection down" })
-vim.keymap.set("v", "<A-S-Up>", ":<C-u>'<,'>t'<-1<CR>gv", { desc = "Duplicate selection up" })
-vim.keymap.set("v", "<A-S-j>", "<cmd>t'><cr>gv", { desc = "Duplicate selection down" })
-vim.keymap.set("v", "<A-S-k>", ":<C-u>'<,'>t'<-1<CR>gv", { desc = "Duplicate selection up" })
-
-vim.keymap.set("i", "<A-S-Down>", "<Esc>:t.<CR>gi", { desc = "Duplicate line down" })
-vim.keymap.set("i", "<A-S-Up>", "<Esc>:t-1<CR>gi", { desc = "Duplicate line up" })
-vim.keymap.set("i", "<A-S-j>", "<Esc>:t.<CR>gi", { desc = "Duplicate line down" })
-vim.keymap.set("i", "<A-S-k>", "<Esc>:t-1<CR>gi", { desc = "Duplicate line up" })
 
 -- Select all
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
@@ -107,7 +58,8 @@ vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines" })
 vim.keymap.set("n", "{", "{zz", { desc = "Previous paragraph and center" })
 vim.keymap.set("n", "}", "}zz", { desc = "Next paragraph and center" })
 
--- Split windows
+-- Split windows (LazyVim uses <leader>| and <leader>-)
+-- Keeping these as alternatives
 vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<cr>", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<cmd>split<cr>", { desc = "Split window horizontally" })
 
@@ -117,11 +69,15 @@ vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Navigate to lower split" })
 vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Navigate to left split" })
 vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Navigate to right split" })
 
--- Resize splits with Option/Alt + Shift + arrows
-vim.keymap.set("n", "<A-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-vim.keymap.set("n", "<A-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<A-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<A-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+-- Resize splits (using leader key for reliability)
+vim.keymap.set("n", "<leader>+", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<leader>-", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<leader>>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+vim.keymap.set("n", "<leader><", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+
+-- Duplicate lines (Ctrl+D in insert mode, avoiding conflicts)
+vim.keymap.set("n", "<leader>d", "<cmd>t.<cr>", { desc = "Duplicate line down" })
+vim.keymap.set("v", "<leader>d", "<cmd>t'><cr>gv", { desc = "Duplicate selection down" })
 
 -- Quick buffer navigation
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
@@ -131,3 +87,32 @@ vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" 
 vim.keymap.set("n", "c", '"_c', { desc = "Change without yank" })
 vim.keymap.set("n", "C", '"_C', { desc = "Change to end of line without yank" })
 vim.keymap.set("v", "c", '"_c', { desc = "Change without yank" })
+
+-- Obsidian.nvim keymaps
+-- Main commands
+vim.keymap.set("n", "<leader>on", "<cmd>Obsidian new<cr>", { desc = "Obsidian: New note" })
+vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian quick_switch<cr>", { desc = "Obsidian: Quick switch" })
+vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<cr>", { desc = "Obsidian: Search" })
+vim.keymap.set("n", "<leader>of", "<cmd>Obsidian follow_link<cr>", { desc = "Obsidian: Follow link" })
+
+-- Daily notes
+vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian today<cr>", { desc = "Obsidian: Today's note" })
+vim.keymap.set("n", "<leader>oy", "<cmd>Obsidian yesterday<cr>", { desc = "Obsidian: Yesterday's note" })
+vim.keymap.set("n", "<leader>om", "<cmd>Obsidian tomorrow<cr>", { desc = "Obsidian: Tomorrow's note" })
+
+-- Navigation
+vim.keymap.set("n", "<leader>ob", "<cmd>Obsidian backlinks<cr>", { desc = "Obsidian: Backlinks" })
+vim.keymap.set("n", "<leader>ol", "<cmd>Obsidian links<cr>", { desc = "Obsidian: Links in current note" })
+vim.keymap.set("n", "<leader>oc", "<cmd>Obsidian toc<cr>", { desc = "Obsidian: Table of contents" })
+
+-- Visual mode commands
+vim.keymap.set("v", "<leader>ol", "<cmd>Obsidian link<cr>", { desc = "Obsidian: Link selection" })
+vim.keymap.set("v", "<leader>on", "<cmd>Obsidian link_new<cr>", { desc = "Obsidian: New note from selection" })
+vim.keymap.set("v", "<leader>oe", "<cmd>Obsidian extract_note<cr>", { desc = "Obsidian: Extract to new note" })
+
+-- Other useful commands
+vim.keymap.set("n", "<leader>or", "<cmd>Obsidian rename<cr>", { desc = "Obsidian: Rename note" })
+vim.keymap.set("n", "<leader>op", "<cmd>Obsidian paste_img<cr>", { desc = "Obsidian: Paste image" })
+vim.keymap.set("n", "<leader>ox", "<cmd>Obsidian toggle_checkbox<cr>", { desc = "Obsidian: Toggle checkbox" })
+vim.keymap.set("n", "<leader>oT", "<cmd>Obsidian template<cr>", { desc = "Obsidian: Insert template" })
+vim.keymap.set("n", "<leader>og", "<cmd>Obsidian tags<cr>", { desc = "Obsidian: Search tags" })
