648d7f4860dbc9426d81b7e2d32f25eb93a43cbd
Tmeister <noone@tmeister.net>
2025-12-06 10:18:11 -0600

    add 1.66 spot

--- hypr/.config/hypr/scripts/resolution-toggle.sh
+++ hypr/.config/hypr/scripts/resolution-toggle.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Resolution toggle script for dual monitor setup
-# Cycles through scales: 1.25 -> 1.5 -> 2.0 -> 1.25
+# Cycles through scales: 1.5 -> 1.666667 -> 2.0 -> 1.5
 
 # Get current scale of DP-1 specifically
 CURRENT_SCALE=$(hyprctl monitors -j | jq -r '.[] | select(.name == "DP-1") | .scale')
 
 case "$CURRENT_SCALE" in
-    1.25*)
-        NEXT_SCALE=1.5
-        GDK_VALUE=1.5
-        HDMI_Y=1440
-        ;;
     1.5*)
+        NEXT_SCALE=1.666667
+        GDK_VALUE=1.5
+        HDMI_Y=1296
+        ;;
+    1.6*)
         NEXT_SCALE=2.0
         GDK_VALUE=2
         HDMI_Y=1080
         ;;
     2*)
-        NEXT_SCALE=1.25
-        GDK_VALUE=1.25
-        HDMI_Y=1728
+        NEXT_SCALE=1.5
+        GDK_VALUE=1.5
+        HDMI_Y=1440
         ;;
     *)
         NEXT_SCALE=1.5
